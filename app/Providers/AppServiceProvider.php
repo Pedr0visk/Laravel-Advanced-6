35eafb148699a19b071a705faf27cfef951d5f8e
pbelloto <pbelloto1997@gmail.com>
2020-01-15 20:22:25 -0300

    cleared

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Billing\PaymentGatewayContract;
-use App\Billing\BankPaymentGateway;
-use App\Billing\CreditPaymentGateway;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,14 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(PaymentGatewayContract::class, function ($app) {
-
-            if (request()->has('credit')) {
-                return new CreditPaymentGateway('usd');
-            }
-
-            return new BankPaymentGateway('usd');
-        });
+        //
     }
 
     /**
